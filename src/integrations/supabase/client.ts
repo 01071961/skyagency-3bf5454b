f0dda523f851b77520e56af2e533789f14cee4ef
01071961 <skyagencysc@gmail.com>
2026-01-22 12:20:43 -0300

    Atualização do projeto Sky Agency

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = "https://pfqhspnyejzohxwaqnyb.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmcWhzcG55ZWp6b2h4d2FxbnliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5NTQ2NjksImV4cCI6MjA4NDUzMDY2OX0.yuFMCxVKrD4we1LlPXWBx3vETi2OIcurG7DjtLSTXB4";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
